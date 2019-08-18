3b7009b7886c9967697352835a96d4825371b2a7
Calvin Sung <Calvin.Sung@edf-re.com>
2019-08-18 10:15:19 -0700

    Rock Paper Scissor changes

--- Code_Challenges/RockPaperScissors.py
+++ Code_Challenges/RockPaperScissors.py
@@ -1,12 +1,16 @@
 #This code generates a random number between 1 and 10, then asks the user to guess the number.  Will let user know if guess is too low, high, or exactly right.  Keeps track of guesses
 #User can type "exit" to exit.
 import random
-a = random.randint(1, 10)
+lownumber = 1
+highnumber = 10
+a = random.randint(lownumber, highnumber)
 guesses = 0
+
+print("There is a number between ", lownumber, "and ", highnumber, ".")
 
 while True:
     
-    playerguess = int(input("Guess the number generated."))
+    playerguess = int(input("Guess the number generated in between."))
     
     if playerguess == a:
         print("You got the number!")
