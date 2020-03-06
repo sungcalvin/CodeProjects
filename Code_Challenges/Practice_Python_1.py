672e26fe971c86d56870a6c9d8daa8405e7be6e4
Calvin Sung <Calvin.Sung@edf-re.com>
2020-03-05 20:41:14 -0800

    For and While Loops

--- Code_Challenges/Practice_Python_1.py
+++ Code_Challenges/Practice_Python_1.py
@@ -1,51 +1,45 @@
-
-#Conditions and Branching
-
-#Use equality operator to check if two values are equal
-print(6 == 7)
-print("\n")
-
-i = 6
-print(i >= 5)
-print(i > 5)
-print("\n")
-
-#Use inequality operator to check if two values are not equal
-i = 7
-print(i != 6)
-print("\n")
-
-#Use equality operator to check if two strings are the same
-print('a' == 'A')
-print("\n")
-
-#Branching allows us to run different statements for different input
-age = 18
-if (age > 18):
-
-    print("you can enter")
-
-elif(age == 18):
-    
-    print("go see Pink Floyd")
-
-else:
-
-    print("sorry, too young")
-
-print("move on")
-
-print("\n")
-
-#Logic operations
-
-#Not operator exchanges TRUE and FALSE
-print(not(True))
-
-print("\n")
-
-
-
 #Loops
 
+#Range function (Output is Python 2.0 only)
+
+#Outputs an ordered sequence given an index input
+print(range(3))
+range(10,15)
+print(range(10,15))
+
+
+#For loops
+
+#Start with a list "squares" showing colors.
+squares = ["red", "yellow", "green", "purple", "blue"]
+
+print("Squares is now", squares)
+
+#For loop to set these colors to "white"
+for i in range(0,5):
+    squares[i] = "white"
+
+print("Squares is now", squares)
+
+#For loop to print list squares, without needing indices
+squares = ["red", "yellow", "green", "purple", "blue"]
+
+for square in squares:
+
+    print(square)
+
 print("\n")
+
+#Syntax to iterate throug a list and provide the index of each element
+squares = ["red", "yellow", "green", "purple", "blue"]
+
+#Variable "i" is the index, "square" the corresponding element in the list
+for i, square in enumerate(squares):
+    
+    print(i, square)
+
+print("\n")
+
+
+#While loops are similar to for loops
+
