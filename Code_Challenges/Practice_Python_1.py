a18b2ea0747b228d6a93659ac26c1f51d393bc4b
Calvin Sung <sung.calvin@gmail.com>
2020-03-25 08:16:03 -0700

    Objects and Classes

--- Code_Challenges/Practice_Python_1.py
+++ Code_Challenges/Practice_Python_1.py
@@ -1,79 +1,44 @@
-#####Functions
+#####Objects and Classes
 
-##Some basic functions
 
-#The function "len" returns the length of a list
-print("The length of this list is: ", len(['A', 'B', 1]))
-print("\n")
 
-#This function "sum" returns the sum of all the elements in a list
-album_ratings = [10.0, 8.5, 9.5, 7.0]
-album_ratings_sum = sum(album_ratings)
-print("The sum of list album_ratings is, ", album_ratings_sum)
-print("\n")
-
-#This function "sorted" returns a new sorted list or tuple without affecting the original
-album_ratings = [10.0, 8.5, 9.5, 7.0]
-sorted_album_rating = sorted(album_ratings)
-print("Sorted album rating is ", sorted_album_rating)
-print("Album rating is unchanged ", album_ratings)
-print("\n")
-
-#This list method "sort" returns the 
-album_ratings = [10.0, 8.5, 9.5, 7.0]
-album_ratings.sort()
-print("List album-ratings sorted using list method .sort() is ", album_ratings)
-print("\n")
-
-##Some user-defined functions
-
-#Sometimes a function will not return anything.  Example:
-def MJ():
-    print("Michael Jackson is here!")
-
-#Function call
-MJ()
-print("\n")
-
-#This function takes an input a as an argument and adds one to it.  
-def add1(a):
-    """Add 1 to a.
+class Circle(object):
+    '''
+        Intro:
+    	
+        Every object has 
+		○ A type
+		○ An internal data representation (a blueprint)
+		○ A set of procedures for interacting with the object (methods)
+	    
+        An object is an instance of a particular type.
         
-        Type more here in this code block!
+        Methods: A class or type's methods are functions that every instance of that class or type provides.  Example: "List.sort()".  Methods change or interact with the object
 
-    """
-    b = a + 1;
-    print(a, " + 1 = ", b)
-    return b
+        
 
-add1(1131)
-print("\n")
+    '''
 
-#This function prints out in sequence the index and contents of an enumerable parameter
-def printStuff(Stuff):
-    for i,s in enumerate(Stuff):
-        print("Album ", i, "Rating is ", s)
+    def __init__(self, radius=3, color='red'):
+        '''
+        Note: You need two underscores on either side of the init
+        Note: You do not need to specify "self" as a parameter when invoking a method it's in.  Python will take care of that.
+        Note: Default values have been specified for the parameters.
+        '''
+        self.radius = radius
+        self.color = color
+
+    def add_radius(self, r):
+        self.radius = self.radius + r
+        return (self.radius)
+
     
-album_ratings = [10.0, 9.5, 8.5]
-printStuff(album_ratings)
-print("\n")
 
-#This function takes a sequence of names as an argument and prints them out one at a time.  
-def ArtistNames(*names):
-    """
-    ArtistNames prints the parameters given
+#Call a circle object
+newCircle = Circle(5, 'purple')
+newCircle.add_radius(2)
+print(newCircle.radius)
 
-    Note: This also makes use of a loop
-
-    """
-    for name in names:
-        print(name)
-
-ArtistNames("Michael Jackson", "AC / DC", "Pink Floyd")
-
-ArtistNames("Michael Jackson", "AC / DC")
-
-ArtistNames(1,2,3,4)
-print("\n")
-
+#The 'dir' function is useful for obtaining all the data attributes and methods of a class.  Example: dir(NameofObject):
+print(dir(Circle))
 
