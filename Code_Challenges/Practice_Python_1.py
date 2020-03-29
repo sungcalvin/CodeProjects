15dde95eb1774049b27b9fdbc0ea05718005781c
Calvin Sung <sung.calvin@gmail.com>
2020-03-28 18:20:47 -0700

    Reading Files with Open

--- Code_Challenges/Practice_Python_1.py
+++ Code_Challenges/Practice_Python_1.py
@@ -1,44 +1,56 @@
-#####Objects and Classes
+#####Reading Files with Open
 
 
+'''
+When opening a file, use the open function.  
 
-class Circle(object):
-    '''
-        Intro:
-    	
-        Every object has 
-		○ A type
-		○ An internal data representation (a blueprint)
-		○ A set of procedures for interacting with the object (methods)
-	    
-        An object is an instance of a particular type.
-        
-        Methods: A class or type's methods are functions that every instance of that class or type provides.  Example: "List.sort()".  Methods change or interact with the object
+Arguments:
+1) Filepath: Filename (note can use "" for a filename in the same directory)
+2) Mode: r for read, w for write, a for append
+3) Store in a file object
 
-        
+Always close the file with the close() statement.
 
-    '''
+More efficient to use a with statement to open a file.  The advantage is it automatically closes the file.
 
-    def __init__(self, radius=3, color='red'):
-        '''
-        Note: You need two underscores on either side of the init
-        Note: You do not need to specify "self" as a parameter when invoking a method it's in.  Python will take care of that.
-        Note: Default values have been specified for the parameters.
-        '''
-        self.radius = radius
-        self.color = color
+'''
 
-    def add_radius(self, r):
-        self.radius = self.radius + r
-        return (self.radius)
+#Follow the below format for open()
+#Better practice to use a with statement to open a file because it automatically closes the file.
+#The code will perform all directions in the indent block, then close the file at the end of the indent
+with open("exampleFile.txt", "r") as File1:
+    
+    file_stuff = File1.read()
+
+    print(file_stuff)
+
+#You can check if a file is closed, but you cannot read from it from outside the indent.
+print(File1.closed)
+
+#However, you can print the file contents from outside the indent.
+print(file_stuff)
+
+
+#The readline() method can be called sequentially to read the next sequence.  Python will know to start a new line
+#The same can be done with elements in a list.
+with open("exampleFile.txt", "r") as File1:
+    file_stuff = File1.readline()
+    print(file_stuff)
+    file_stuff = File1.readline()
+    print(file_stuff)
+
+#We can use a loop to print out each line.
+with open("exampleFile.txt", "r") as File1:
+    for line in File1:
+        print(line)
+
+#You can specify the number of characters you'd like to read from a string as an argument to readlines()
+with open("exampleFile.txt", "r") as File1:
+    file_stuff = File1.readlines(4)
+    print(file_stuff)
+    file_stuff = File1.readlines(5)
+    print(file_stuff)
+    file_stuff = File1.readlines(9)
+    print(file_stuff)
 
     
-
-#Call a circle object
-newCircle = Circle(5, 'purple')
-newCircle.add_radius(2)
-print(newCircle.radius)
-
-#The 'dir' function is useful for obtaining all the data attributes and methods of a class.  Example: dir(NameofObject):
-print(dir(Circle))
-
